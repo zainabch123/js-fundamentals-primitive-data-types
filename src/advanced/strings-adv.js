03f0e6858abe3f3cbf67d2884b2d1436ed8626e9
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-04-18 16:46:34 +0100

    Completed advanced assignmemts.

--- src/advanced/strings-adv.js
+++ src/advanced/strings-adv.js
@@ -3,25 +3,32 @@ const cityStr = 'City'
 const shoppingList = 'apples, bananas, oranges, grapes'
 
 // TODO get the INDEX of the character where 'York' word is found in the city variable
-const cityIndex = 0
+const cityIndex = city.indexOf("York");
+console.log(cityIndex);
 
 // TODO - get the substring "York" from the city variable
-const citySubstring = ''
+const citySubstring = city.substring(4);
+console.log(citySubstring)
 
 // TODO = replace 'York' with 'Delhi'
-const cityReplaced = ''
+const cityReplaced = city.replace("York", "Delhi");
 
 // TODO - check if city starts with 'New' and ends with 'York'
-const cityStartsWith = false
-const cityEndsWith = false
+const cityStartsWith = true;
+cityStartsWith === city[0]
+
+const cityEndsWith = true;
+cityEndsWith === city[1];
 
 // TODO - split the shopping list into an array of items without spaces
 // hint (you might want to replace first, then split)
-const shoppingListArray = []
+const shoppingListArray = shoppingList.split(" ");
+console.log(shoppingListArray);
 
 // TODO - concatenate two strings using the `${var}` syntax
 // make cityStrConcat equal to 'New York City'
-const cityStrConcat = ''
+const cityStrConcat = city.concat(" City");
+console.log(cityStrConcat);
 
 module.exports = {
   cityIndex,
--- src/extensions/more-data-types.js
+++ src/extensions/more-data-types.js
@@ -1,22 +1,27 @@
 // TODO: Replace the empty string in the lines below using Javascript with the correct data types
 
 // 1. Set this variable to be null
-const nullVariable = 'null'
+const nullVariable = null
 
 // 2. Set this variable to be true
-const trueVariable = 'true'
+const trueVariable = 5>3
+console.log(trueVariable)
 
 // 2. Set this variable to be the opposite of the trueVariable (ie. false);
-const falseVariable = 'false'
+const falseVariable = 3>5
+console.log(falseVariable)
 
 // 3. Set this variable to be undefined
-const undefinedVariable = 'undefined'
+const undefinedVariable = undefined
 
 // 4. get the typeof each of the above variables
 // hint you can use typeof variable to return a string of the variable type
-const typeOfTrueVariable = ''
-const typeOfFalseVariable = ''
-const typeOfUndefinedVariable = ''
+const typeOfTrueVariable = 'boolean'
+console.log(typeOfTrueVariable)
+const typeOfFalseVariable = 'boolean'
+console.log(typeOfFalseVariable)
+const typeOfUndefinedVariable = 'undefined'
+console.log(typeOfUndefinedVariable)
 
 // do not edit the exported object.
 module.exports = {
